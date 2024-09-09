21175267ce66047ea6158cc6923473b543c6a431
BouleJaune <amthierry81@live.fr>
2024-09-10 00:04:36 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://boulejaune.github.io/",
-  // base: "portfolio",
+  base: "portfolio",
   integrations: [
     sitemap(),
     robotsTxt({
