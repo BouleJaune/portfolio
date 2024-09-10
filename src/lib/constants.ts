d7f3348d7ca4435d6bd56adcc3578ef8a1ccfcb0
BouleJaune <amthierry81@live.fr>
2024-09-10 12:07:24 +0200

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const LINKS = {
   github: "https://github.com/BouleJaune/",
   linkedin: "https://www.linkedin.com/in/thierry-amettler-b2408719b/",
+  mail: "mailto:thierry.amettler@proton.me",
 };
 
 export const loaderAnimation = [
